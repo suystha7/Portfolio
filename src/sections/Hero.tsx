21edc14a835edd53358253f37bff91ac6614a270
suyogstha7 <suyogstha317@gmail.com>
2025-02-13 13:26:09 +0545

    design header & hero section

--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -1,3 +1,97 @@
+import Image from "next/image";
+import memojiImage from "@/assets/images/memoji-computer.png";
+import ArrowDown from "@/assets/icons/arrow-down.svg";
+import grainImage from "@/assets/images/grain.jpg";
+import Star from "@/assets/icons/star.svg";
+import Sparkle from "@/assets/icons/sparkle.svg";
+import HeroOrbit from "@/components/HeroOrbit";
+
 export const HeroSection = () => {
-  return <div>Hero Section</div>;
+  return (
+    <div className="py-32 md:py-48 lg:py-60 relative z-0 overflow-x-clip">
+      <div className="absolute inset-0 mask-gradient">
+        <div
+          className="absolute inset-0 -z-30 opacity-5"
+          style={{
+            backgroundImage: `url(${grainImage.src})`,
+          }}
+        ></div>
+
+        {/* Hero Rings */}
+        <div className="size-[620px] hero-ring"></div>
+        <div className="size-[820px] hero-ring"></div>
+        <div className="size-[1020px] hero-ring"></div>
+        <div className="size-[1220px] hero-ring"></div>
+
+        {/* Hero Orbit Elements */}
+        <HeroOrbit size={800} rotation={-72}>
+          <Star className="size-28 text-emerald-300" />
+        </HeroOrbit>
+        <HeroOrbit size={550} rotation={20}>
+          <Star className="size-12 text-emerald-300" />
+        </HeroOrbit>
+        <HeroOrbit size={590} rotation={98}>
+          <Star className="size-8 text-emerald-300" />
+        </HeroOrbit>
+        <HeroOrbit size={550} rotation={20}>
+          <Star className="size-12 text-emerald-300" />
+        </HeroOrbit>
+        <HeroOrbit size={430} rotation={-14}>
+          <Sparkle className="size-8 text-emerald-300/20" />
+        </HeroOrbit>
+        <HeroOrbit size={440} rotation={79}>
+          <Sparkle className="size-5 text-emerald-300/20" />
+        </HeroOrbit>
+        <HeroOrbit size={530} rotation={178}>
+          <Sparkle className="size-10 text-emerald-300/20" />
+        </HeroOrbit>
+        <HeroOrbit size={710} rotation={144}>
+          <Sparkle className="size-14 text-emerald-300/20" />
+        </HeroOrbit>
+        <HeroOrbit size={720} rotation={85}>
+          <div className="size-3 rounded-full bg-emerald-300/20" />
+        </HeroOrbit>
+        <HeroOrbit size={520} rotation={-41}>
+          <div className="size-3 rounded-full bg-emerald-300/20" />
+        </HeroOrbit>
+        <HeroOrbit size={650} rotation={-5}>
+          <div className="size-2 rounded-full bg-emerald-300/20" />
+        </HeroOrbit>
+      </div>
+
+      <div className="container">
+        <div className="flex flex-col items-center">
+          <Image src={memojiImage} className="size-[100px]" alt="memoji" />
+          <div className="bg-gray-950 border border-gray-800 px-4 py-1.5 inline-flex items-center gap-4 rounded-lg">
+            <div className="bg-green-500 size-2.5 rounded-full"></div>
+            <div className="text-sm font-medium">
+              Available for new projects
+            </div>
+          </div>
+
+          <div className="max-w-lg mx-auto">
+            <h1 className="text-3xl md:text-5xl font-serif text-center mt-8 tracking-wide">
+              Building Exceptional User Experiences
+            </h1>
+            <p className="mt-4 text-center text-white/60 md:text-lg">
+              I specialize in transforming designs into functional,
+              high-performing web applications. Let&apos;s discuss your next
+              project.
+            </p>
+          </div>
+
+          <div className="flex flex-col md:flex-row justify-center items-center mt-8 gap-4">
+            <button className="inline-flex items-center gap-2 border border-white/15 px-6 h-12 rounded-xl">
+              <span className="font-semibold">Explore my work</span>
+              <ArrowDown className="size-4" />
+            </button>
+            <button className="inline-flex items-center gap-2 border border-white bg-white text-gray-900 px-6 h-12 rounded-xl">
+              <span>👋</span>
+              <span className="font-semibold">Let&apos;s Connect</span>
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
